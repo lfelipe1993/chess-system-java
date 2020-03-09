42c8ec2332e8d256d3ce3dac559f6356f098bc9b
Luiz Felipe Mecina Greijo <lfelipe1993@live.com>
2020-03-09 19:35:08 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,9 +1,9 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
 		this.position = null;
@@ -12,6 +12,27 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
-	
+
+	public abstract boolean[][] PossibleMoves();
+
+	// Patern TemplateMethod
+	public boolean PossibleMove(Position position) {
+		return PossibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean IsThereAnyPossibleMove() {
+		boolean[][] mat = PossibleMoves();
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+
+	}
+
 }
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -16,4 +16,10 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	@Override
+	public boolean[][] PossibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
